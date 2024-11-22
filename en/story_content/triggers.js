c293f75548682ed2e93e799b6dd791709137a70e
xpandsim <xpandsim@gmail.com>
2024-11-22 11:24:14 +0300

    Add files via upload

---
+++ en/story_content/triggers.js
@@ -0,0 +1,102 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6F6iRm7bP5X":
+        Script1();
+        break;
+      case "68a0njF61XR":
+        Script2();
+        break;
+      case "60DgDYql8Fk":
+        Script3();
+        break;
+      case "6T6Gjn2Pj9P":
+        Script4();
+        break;
+      case "5fIdg7f8Ul3":
+        Script5();
+        break;
+      case "6ahU9zBteC8":
+        Script6();
+        break;
+      case "6NynuSw1xhJ":
+        Script7();
+        break;
+      case "69YPZsIsTkA":
+        Script8();
+        break;
+      case "5ysmvgmknJK":
+        Script9();
+        break;
+      case "6hrGPJWjx5r":
+        Script10();
+        break;
+      case "6O0IVX6KiZ3":
+        Script11();
+        break;
+      case "6fQhEoesXGT":
+        Script12();
+        break;
+      case "5jnXifUYQNz":
+        Script13();
+        break;
+      case "6QE8FawR5DK":
+        Script14();
+        break;
+      case "6cOdh3xGNIz":
+        Script15();
+        break;
+      case "6haxHsFIrmB":
+        Script16();
+        break;
+      case "5oXwvySR6RY":
+        Script17();
+        break;
+      case "6p3dC8wWDJ1":
+        Script18();
+        break;
+      case "6aFARJTRMoL":
+        Script19();
+        break;
+      case "5fkQiXRJAUo":
+        Script20();
+        break;
+      case "65LJ5xXlYuz":
+        Script21();
+        break;
+      case "5pB40qtbOlY":
+        Script22();
+        break;
+      case "6jsmTnZQRmt":
+        Script23();
+        break;
+      case "5cKPv2k9fuF":
+        Script24();
+        break;
+      case "6Bn9SlS4a0p":
+        Script25();
+        break;
+      case "6M8W6iN72Xi":
+        Script26();
+        break;
+      case "5dIFj8w6Cjz":
+        Script27();
+        break;
+      case "6VrDlcrTgnZ":
+        Script28();
+        break;
+      case "5YyvnWdMWh9":
+        Script29();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
